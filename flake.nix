FreesmTeam/FreesmLauncher

feat(nix): jvmPack toplevel function init
Signed-off-by: s0me1newithhand7s <git+me@hand7s.org>

flake.nix
@@ -38,24 +38,64 @@
     ];
 
     forEachSystem = nixpkgs.lib.genAttrs systems;
+
+    mkJvmPack = pkgs: let
+      openjdk = with pkgs; [
+        openjdk8
+        openjdk17
+        openjdk21
+        openjdk25
+      ];
+
+      temurin = with pkgs.javaPackages.compiler.temurin-bin; [
+        jdk-8
+        jdk-17
+        jdk-21
+        jdk-25
+      ];
+
+      corretto = with pkgs.javaPackages.compiler; [
+        corretto17
+        corretto21
+        corretto25
+      ];
+
+      graal-ce = with pkgs.graalvmPackages; [
+        graalvm-ce
+      ];
+
+      graal-unfree = with pkgs.graalvmPackages; [
+        graalvm-oracle_17
+        graalvm-oracle_25
+      ];
+
+    in {
+      inherit openjdk temurin corretto graal-ce graal-unfree;
+      allPack = openjdk ++ temurin ++ corretto ++ graal-ce ++ graal-unfree;
+    };
+
   in {
     overlays.default = final: prev: {
       freesmlauncher-unwrapped = final.callPackage ./nix/unwrapped.nix {
         inherit nix-filter libnbtplusplus self;
       };
 
-      freesmlauncher = final.callPackage ./nix/wrapper.nix;
+      freesmlauncher = final.callPackage ./nix/wrapper.nix {
+        jvmPack = mkJvmPack final;
+      };
     };
 
     packages = forEachSystem (system: let
       pkgs = import nixpkgs {inherit system;};
+
+      jvmPack = mkJvmPack pkgs;
 
       freesmlauncher-unwrapped = pkgs.callPackage ./nix/unwrapped.nix {
         inherit nix-filter libnbtplusplus self;
       };
 
       freesmlauncher = pkgs.callPackage ./nix/wrapper.nix {
-        inherit freesmlauncher-unwrapped;
+        inherit freesmlauncher-unwrapped jvmPack;
       };
 
       freesmlauncher-unwrapped-debug = freesmlauncher-unwrapped.overrideAttrs {
@@ -67,7 +107,7 @@
         freesmlauncher-unwrapped = freesmlauncher-unwrapped-debug;
       };
     in {
-      inherit freesmlauncher freesmlauncher-unwrapped freesmlauncher-debug freesmlauncher-unwrapped-debug;
+      inherit freesmlauncher freesmlauncher-unwrapped freesmlauncher-debug freesmlauncher-unwrapped-debug jvmPack;
 
       default = freesmlauncher;
     });

oleksandr1811/HalkyLauncher

flake.nix
@@ -38,24 +38,64 @@
     ];
 
     forEachSystem = nixpkgs.lib.genAttrs systems;
+
+    mkJvmPack = pkgs: let
+      openjdk = with pkgs; [
+        openjdk8
+        openjdk17
+        openjdk21
+        openjdk25
+      ];
+
+      temurin = with pkgs.javaPackages.compiler.temurin-bin; [
+        jdk-8
+        jdk-17
+        jdk-21
+        jdk-25
+      ];
+
+      corretto = with pkgs.javaPackages.compiler; [
+        corretto17
+        corretto21
+        corretto25
+      ];
+
+      graal-ce = with pkgs.graalvmPackages; [
+        graalvm-ce
+      ];
+
+      graal-unfree = with pkgs.graalvmPackages; [
+        graalvm-oracle_17
+        graalvm-oracle_25
+      ];
+
+    in {
+      inherit openjdk temurin corretto graal-ce graal-unfree;
+      allPack = openjdk ++ temurin ++ corretto ++ graal-ce ++ graal-unfree;
+    };
+
   in {
     overlays.default = final: prev: {
       HalkyLauncher-unwrapped = final.callPackage ./nix/unwrapped.nix {
         inherit nix-filter libnbtplusplus self;
       };
 
-      HalkyLauncher = final.callPackage ./nix/wrapper.nix;
+      HalkyLauncher = final.callPackage ./nix/wrapper.nix {
+        jvmPack = mkJvmPack final;
+      };
     };
 
     packages = forEachSystem (system: let
       pkgs = import nixpkgs {inherit system;};
+
+      jvmPack = mkJvmPack pkgs;
 
       HalkyLauncher-unwrapped = pkgs.callPackage ./nix/unwrapped.nix {
         inherit nix-filter libnbtplusplus self;
       };
 
       HalkyLauncher = pkgs.callPackage ./nix/wrapper.nix {
-        inherit HalkyLauncher-unwrapped;
+        inherit HalkyLauncher-unwrapped jvmPack;
       };
 
       HalkyLauncher-unwrapped-debug = HalkyLauncher-unwrapped.overrideAttrs {
@@ -65,9 +105,10 @@
 
       HalkyLauncher-debug = pkgs.callPackage ./nix/wrapper.nix {
         HalkyLauncher-unwrapped = HalkyLauncher-unwrapped-debug;
+        inherit jvmPack;
       };
     in {
-      inherit HalkyLauncher HalkyLauncher-unwrapped HalkyLauncher-debug HalkyLauncher-unwrapped-debug;
+      inherit HalkyLauncher HalkyLauncher-unwrapped HalkyLauncher-debug HalkyLauncher-unwrapped-debug jvmPack;
 
       default = HalkyLauncher;
     });