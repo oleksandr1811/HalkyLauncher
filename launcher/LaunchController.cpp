d2ce1e74f8d481d09284d50ec0d992121a6f7c96
Alex Popov <123168991+oleksandr1811@users.noreply.github.com>
2026-08-27 14:46:58 +0000

    Freesm update: fix: do not open account select dialog if there are no valid accounts
    Original author: Octol1ttle <l1ttleofficial@outlook.com>
    
    (ported from upstream commit 6ad1bdb77)

--- launcher/LaunchController.cpp
+++ launcher/LaunchController.cpp
@@ -111,7 +111,7 @@ void LaunchController::decideAccount()
         }
     }
 
-    if (!m_accountToUse) {
+    if (!m_accountToUse && accounts->anyAccountIsValid()) {
         // If no default account is set, ask the user which one to use.
         ProfileSelectDialog selectDialog(tr("Which account would you like to use?"), ProfileSelectDialog::GlobalDefaultCheckbox,
                                          m_parentWidget);
