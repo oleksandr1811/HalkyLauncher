21cf20be2c4503372c08555d0c357769fe273395
oleksandr1811 <benenecraftserver@gmail.com>
2026-09-11 18:36:15 +0300

    remove temp file

--- test_compile.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open("launcher/ui/widgets/HalkyNavBar.cpp", "r", encoding="utf-8") as f:
-    content = f.read()
-
-if "#include <QAbstractAnimation>" not in content:
-    content = content.replace("#include <QVariantAnimation>", "#include <QVariantAnimation>\n#include <QAbstractAnimation>")
-    with open("launcher/ui/widgets/HalkyNavBar.cpp", "w", encoding="utf-8") as f:
-        f.write(content)
